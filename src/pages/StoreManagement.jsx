70005f513b0a6f5773202755fa0d3facffe78b38
Mr Zeku <mrzeku12@gmail.com>
2026-01-28 18:44:52 +0000

    File changes

--- src/pages/StoreManagement.jsx
+++ src/pages/StoreManagement.jsx
@@ -42,7 +42,6 @@ export default function StoreManagement() {
     mutationFn: (data) => base44.entities.Store.create(data),
     onSuccess: (newStore) => {
       queryClient.invalidateQueries({ queryKey: ['stores', userEmail] });
-      setFormData({ name: '', description: '' });
       setShowCreateForm(false);
       setSelectedStore(newStore);
     },
